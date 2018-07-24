92c5899eab80492316b5b960400d32d7056315c4
Bryant G. Ly <bryantly@linux.ibm.com>
2018-07-24 10:30:48 -0500

    ibmvsm: Add basic init functions

--- ibmvsm/ibmvsm.c
+++ ibmvsm/ibmvsm.c
@@ -8,10 +8,226 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/poll.h>
+#include <linux/init.h>
+#include <linux/io.h>
+#include <linux/miscdevice.h>
+
+#include <asm/hvcall.h>
+#include <asm/vio.h>
 
 #include "ibmvsm.h"
 
 #define IBMVSM_DRIVER_VERSION "0.1"
+#define MSG_HI	0
+#define MSG_LOW	1
+
+static const char ibmvsm_driver_name[] = "ibmvsm";
+
+static struct crq_server_adapter ibmvsm_adapter;
+
+enum crq_entry_header {
+	CRQ_FREE = 0x00,
+	CRQ_CMD_RSP = 0x80,
+	CRQ_INIT_MSG = 0xC0
+};
+
+enum crq_init_formats {
+	CRQ_INIT = 0x01,
+	CRQ_INIT_COMPLETE = 0x02
+};
+
+/**
+ * ibmvsm_send_init_message() - send initialization message to the client
+ */
+static long ibmvsm_send_init_msg(struct crq_server_adapter *adapter, u8 type)
+{
+	struct ibmvsm_crq_msg *crq;
+	struct vio_dev *vdev = to_vio_dev(adapter->dev);
+	u64 buffer[2] = { 0 , 0 };
+	long rc;
+
+	crq = (struct ibmvsm_crq_msg *)&buffer;
+	crq->valid = CRQ_INIT_MSG;
+	crq->type = type;
+	rc = h_send_crq(vdev->unit_address,
+			cpu_to_be64(buffer[MSG_HI]),
+			cpu_to_be64(buffer[MSG_LOW]));
+
+	return rc;
+}
+
+/**
+ * ibmvsm_read - Read
+ *
+ * @file:	file struct
+ * @buf:	character buffer
+ * @nbytes:	size in bytes
+ * @ppos:	offset
+ *
+ * Return:
+ *	0 - Success
+ *	Non-zero - Failure
+ */
+static ssize_t ibmvsm_read(struct file *file, char *buf, size_t nbytes,
+			   loff_t *ppos)
+{
+	return 0;
+}
+
+/**
+ * ibmvsm_write - Write
+ *
+ * @file:	file struct
+ * @buf:	character buffer
+ * @count:	count field
+ * @ppos:	offset
+ *
+ * Return:
+ *	0 - Success
+ *	Non-zero - Failure
+ */
+static ssize_t ibmvsm_write(struct file *file, const char *buffer,
+			    size_t count, loff_t *ppos)
+{
+	return 0;
+}
+
+/**
+ * ibmvsm_poll - Poll
+ *
+ * @file:	file struct
+ * @wait:	Poll Table
+ *
+ * Return:
+ *	poll.h return values
+ */
+static unsigned int ibmvsm_poll(struct file *file, poll_table *wait)
+{
+	return 0;
+}
+
+/**
+ * ibmvsm_ioctl - IOCTL
+ *
+ * @session:	ibmvsm_file_session struct
+ * @cmd:	cmd field
+ * @arg:	Argument field
+ *
+ * Return:
+ *	0 - Success
+ *	Non-zero - Failure
+ */
+static long ibmvsm_ioctl(struct file *file,
+			 unsigned int cmd, unsigned long arg)
+{
+	return 0;
+}
+
+/**
+ * ibmvsm_open - Open Session
+ *
+ * @inode:	inode struct
+ * @file:	file struct
+ *
+ * Return:
+ *	0 - Success
+ */
+static int ibmvsm_open(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+
+/**
+ * ibmvsm_close - Close Session
+ *
+ * @inode:	inode struct
+ * @file:	file struct
+ *
+ * Return:
+ *	0 - Success
+ *	Non-zero - Failure
+ */
+static int ibmvsm_close(struct inode *inode, struct file *file)
+{
+	return 0;
+}
+
+static const struct file_operations ibmvsm_fops = {
+	.owner		= THIS_MODULE,
+	.read		= ibmvsm_read,
+	.write		= ibmvsm_write,
+	.poll		= ibmvsm_poll,
+	.unlocked_ioctl	= ibmvsm_ioctl,
+	.open           = ibmvsm_open,
+	.release        = ibmvsm_close,
+};
+
+static int ibmvsm_probe(struct vio_dev *vdev, const struct vio_device_id *id)
+{
+	struct crq_server_adapter *adapter = &ibmvsm_adapter;
+
+	dev_set_drvdata(&vdev->dev, NULL);
+	memset(adapter, 0, sizeof(*adapter));
+	adapter->dev = &vdev->dev;
+
+	dev_info(adapter->dev, "Probe for UA 0x%x\n", vdev->unit_address);
+
+	/* Read DMA Window */
+
+	/* Init CRQ */
+
+	return 0;
+}
+
+static int ibmvsm_remove(struct vio_dev *vdev)
+{
+	struct crq_server_adapter *adapter = dev_get_drvdata(&vdev->dev);
+
+	dev_info(adapter->dev, "Entering remove for UA 0x%x\n",
+		 vdev->unit_address);
+
+	/* Need to release the CRQ here */
+
+	return 0;
+}
+
+static struct vio_device_id ibmvsm_device_table[] = {
+	{ "ibm,vsm", "IBM,vsm" },
+	{ "", "" }
+};
+MODULE_DEVICE_TABLE(vio, ibmvsm_device_table);
+
+static struct vio_driver ibmvsm_driver = {
+	.name        = ibmvsm_driver_name,
+	.id_table    = ibmvsm_device_table,
+	.probe       = ibmvsm_probe,
+	.remove      = ibmvsm_remove,
+};
+
+static struct miscdevice ibmvsm_miscdev = {
+	.name = ibmvsm_driver_name,
+	.minor = MISC_DYNAMIC_MINOR,
+	.fops = &ibmvsm_fops,
+};
+
+static int __init ibmvsm_module_init(void)
+{
+	/* Register Misc device */
+	/* Init data structures */
+	/* vio register */
+
+	return 0;
+}
+
+static void __exit ibmvsm_module_exit(void)
+{
+	pr_info("ibmvsm: module exit\n");
+
+	/* vio unregister */
+	/* misc device de-register */
+}
 
 MODULE_AUTHOR("Bryant G. Ly <bryantly@linux.vnet.ibm.com>");
 MODULE_DESCRIPTION("IBM VSM");
--- ibmvsm/ibmvsm.h
+++ ibmvsm/ibmvsm.h
@@ -10,4 +10,33 @@
 #ifndef IBMVSM_H
 #define IBMVSM_H
 
+struct ibmvsm_crq_msg {
+	u8 valid;		/* RPA Defined */
+	u8 type;		/* ibmvsm msg type */
+	u16 rsvd;
+	u32 rsvd1;
+	u64 rsvd2;
+};
+
+/* an RPA command/response transport queue */
+struct crq_queue {
+	struct ibmvsm_crq_msg *msgs;
+	int size, cur;
+};
+
+/* VSM server adapter settings */
+struct crq_server_adapter {
+	struct device *dev;
+	struct crq_queue queue;
+	u32 liobn;
+	u32 riobn;
+};
+
+#define h_reg_crq(ua, tok, sz) \
+		  plpar_hcall_norets(H_REG_CRQ, ua, tok, sz)
+#define h_free_crq(ua) \
+		   plpar_hcall_norets(H_FREE_CRQ, ua)
+#define h_send_crq(ua, d1, d2) \
+		   plpar_hcall_norets(H_SEND_CRQ, ua, d1, d2)
+
 #endif /* __IBMVSM_H */
